30c855393fe1c6493416a7928900e02def73221b
Mihai Fufezan <mihai@fufexan.net>
2024-10-27 17:01:02 +0200

    Nix: init

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Hyprland QT/qml utility apps";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+
+    hyprutils = {
+      url = "github:hyprwm/hyprutils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+    ...
+  } @ inputs: let
+    inherit (nixpkgs) lib;
+    eachSystem = lib.genAttrs (import systems);
+    pkgsFor = eachSystem (
+      system:
+        import nixpkgs {
+          localSystem = system;
+          overlays = [self.overlays.default];
+        }
+    );
+  in {
+    overlays = import ./nix/overlays.nix {inherit inputs self lib;};
+
+    packages = eachSystem (system: {
+      default = self.packages.${system}.hyprland-qtutils;
+      inherit (pkgsFor.${system}) hyprland-qtutils;
+    });
+  };
+}
---
+++ nix/default.nix
@@ -0,0 +1,52 @@
+{
+  lib,
+  stdenv,
+  cmake,
+  pkg-config,
+  hyprutils,
+  kdePackages,
+  pciutils,
+  qt6,
+  version ? "0",
+}: let
+  inherit (lib.sources) cleanSource cleanSourceWith;
+  inherit (lib.strings) hasSuffix makeBinPath;
+in
+  stdenv.mkDerivation {
+    pname = "hyprland-qtutils";
+    inherit version;
+
+    src = cleanSourceWith {
+      filter = name: _type: let
+        baseName = baseNameOf (toString name);
+      in
+        ! (hasSuffix ".nix" baseName);
+      src = cleanSource ../.;
+    };
+
+    nativeBuildInputs = [
+      cmake
+      pkg-config
+      qt6.wrapQtAppsHook
+    ];
+
+    buildInputs = [
+      hyprutils
+      kdePackages.kirigami-addons
+      qt6.qtbase
+      qt6.qtsvg
+      qt6.qtwayland
+    ];
+
+    preFixup = ''
+      qtWrapperArgs+=(--prefix PATH : "${makeBinPath [pciutils]}")
+    '';
+
+    meta = {
+      description = "Hyprland QT/qml utility apps";
+      homepage = "https://github.com/hyprwm/hyprland-qtlibs";
+      license = lib.licenses.bsd3;
+      maintainers = [lib.maintainers.fufexan];
+      platforms = lib.platforms.linux;
+    };
+  }
